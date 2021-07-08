c16166b0720775201e7fd8e738a9e3fa02dc90b6
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2021-07-08 16:13:55 +0700

    aligning card content

--- components/_pages/index/IsolasiMandiri/index.vue
+++ components/_pages/index/IsolasiMandiri/index.vue
@@ -48,7 +48,7 @@
     <div class="flex flex-col flex-no-wrap sm:flex-row gap-4 mt-4 lg:mt-6 lg:gap-6">
       <ActionCard
         class="w-full lg:w-1/2"
-        title="Konsultasi dengan Dokter<br><br>"
+        title="Konsultasi dengan Dokter"
         body="Bagi Wargi yang membutuhkan obat, yuk konsultasikan terlebih dahulu dengan dokter melalui layanan telekonsultasi dokter Pikobar"
         prompt="Tanyakan Sekarang"
         :event="konsultasiDokterEvent"
