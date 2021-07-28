c22d4d753fde5ef1331b80d2a282610530d944cf
Arif9878 <arifboyz16@gmail.com>
2021-07-28 10:57:31 +0700

    change title bar "Cari Oksigen"

--- pages/oxygen/index.vue
+++ pages/oxygen/index.vue
@@ -72,7 +72,7 @@ export default {
     })
   },
   head () {
-    const title = 'Terapi Oksigen - Pikobar [Pusat Informasi dan Koordinasi COVID-19 Jawa Barat]'
+    const title = 'Cari Oksigen - Pikobar [Pusat Informasi dan Koordinasi COVID-19 Jawa Barat]'
     return {
       title,
       meta: [
