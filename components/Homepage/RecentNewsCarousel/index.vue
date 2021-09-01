d1a9c4a95ee42782c1f86d14fec4bd4e0e743743
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-09-01 16:04:33 +0700

    component: create NewsCarousel wrapper for recent news use case in homepage

---
+++ components/Homepage/RecentNewsCarousel/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <NewsCarousel
+    :items="news"
+    :loading="loading"
+  />
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import { NewsCarousel } from '~/components/NewsCarousel'
+export default {
+  components: {
+    NewsCarousel
+  },
+  computed: {
+    ...mapState({
+      news: (state) => {
+        const items = state.news?.items || []
+        return items.map(item => ({
+          ...item,
+          thumbnail: item.image,
+          date: item.published_at,
+          source: item.news_channel,
+          url: item.route
+        }))
+      }
+    }),
+    loading () {
+      return !Array.isArray(this.news) ||
+        !this.news.length
+    }
+  },
+  mounted () {
+    this.$store.dispatch('news/getItems', {
+      perPage: 4
+    })
+  }
+}
+</script>
