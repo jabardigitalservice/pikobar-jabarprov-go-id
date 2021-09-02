4a5cb51d9539167fc9cbd11e6ec0d45eb735a3d1
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-09-02 20:34:41 +0700

    component (EventStatCard): create skeleton view;

---
+++ components/EventStatCard/EventStatCardSkeleton.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="my-6">
+    <BaseSkeleton
+      :dark="dark"
+      class="w-3/4 h-2 mb-12 rounded-full"
+    />
+    <BaseSkeleton
+      :dark="dark"
+      class="w-1/3 h-2 mb-4 rounded-full"
+    />
+    <BaseSkeleton
+      :dark="dark"
+      class="w-2/5 h-2 mb-8 rounded-full"
+    />
+    <BaseSkeleton
+      :dark="dark"
+      class="w-1/3 h-2 mb-4 rounded-full"
+    />
+    <BaseSkeleton
+      :dark="dark"
+      class="w-2/5 h-2 rounded-full"
+    />
+  </div>
+</template>
+
+<script>
+import BaseSkeleton from '~/components/BaseSkeleton'
+export default {
+  components: {
+    BaseSkeleton
+  },
+  props: {
+    dark: {
+      type: Boolean
+    }
+  }
+}
+</script>
