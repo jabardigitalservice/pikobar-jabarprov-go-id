cb4e9d8b2fbe81fe967d9d19b6055ee464b53453
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-09-07 19:33:59 +0700

    component: create base Container component;

---
+++ components/Base/Container/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <div
+      :class="{
+        'px-4 md:px-6': !noGutters,
+        'container': !fluid,
+        'mx-auto': centered
+      }"
+    >
+      <slot />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    tag: {
+      type: String,
+      default: 'section'
+    },
+    fluid: {
+      type: Boolean
+    },
+    centered: {
+      type: Boolean,
+      default: true
+    },
+    noGutters: {
+      type: Boolean
+    }
+  }
+}
+</script>
