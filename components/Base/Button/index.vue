467799a8c5638937f381d8a3bc17d92d579c04a8
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-09-07 12:17:44 +0700

    component: create base Button component;

---
+++ components/Base/Button/index.vue
@@ -0,0 +1,99 @@
+<template>
+  <component
+    :is="tag"
+    :class="{
+      'base-button': true,
+      'base-button--outlined': outlined,
+      'base-button--shadowed': shadowed
+    }"
+    :disabled="disabled"
+    v-bind="$attrs"
+  >
+    <span class="base-button__label">
+      <slot>
+        {{ label }}
+      </slot>
+    </span>
+    <i class="base-button__icon">
+      <slot name="icon" />
+    </i>
+  </component>
+</template>
+
+<script>
+/**
+ * As of now, only supports green color (filled or outlined).
+ */
+export default {
+  props: {
+    tag: {
+      type: String,
+      default: 'button'
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    outlined: {
+      type: Boolean
+    },
+    disabled: {
+      type: Boolean
+    },
+    shadowed: {
+      type: Boolean
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.base-button {
+  @apply inline-flex flex-row flex-no-wrap gap-2
+  justify-around items-center
+  px-4 py-3
+  border border-solid border-transparent
+  rounded-lg
+  bg-brand-green-darker
+  text-white font-bold tracking-wide;
+
+  &--outlined {
+    @apply border-green-500
+    rounded-md
+    bg-white
+    text-green-700;
+  }
+
+  &--shadowed {
+    @apply shadow-sm;
+  }
+
+  &__label {
+    color: inherit;
+    font-size: inherit;
+    line-height: 1.618;
+
+    @apply flex-none;
+  }
+
+  &__icon {
+    line-height: 0;
+    @apply flex-none
+    inline-flex flex-row flex-no-wrap
+    justify-center items-center;
+  }
+
+  &:hover {
+    @apply opacity-75;
+  }
+
+  &:active {
+    @apply opacity-100;
+  }
+
+  &:focus,
+  &:active {
+    @apply outline-none;
+  }
+}
+</style>
