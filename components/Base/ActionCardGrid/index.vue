31d5cac234497531c19d6f309719564668b4f9fb
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-09-16 00:36:29 +0700

    component: create base wrapper for 2 column action card grid view;

---
+++ components/Base/ActionCardGrid/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="grids">
+    <ActionCard
+      v-for="(action, i) in cards"
+      :key="i"
+      v-bind="action"
+    />
+  </div>
+</template>
+
+<script>
+import ActionCard from '~/components/Base/ActionCard'
+export default {
+  components: {
+    ActionCard
+  },
+  props: {
+    /**
+     * Array of ActionCard's props
+     */
+    cards: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.grids {
+  @apply grid grid-cols-1 gap-4;
+
+  @screen md {
+    @apply grid-cols-2
+    items-stretch;
+  }
+
+  @screen lg {
+    @apply gap-8;
+  }
+}
+</style>
