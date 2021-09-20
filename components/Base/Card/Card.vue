3218f6e34e60dc04c92b56a6faf6f07832ef67e0
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-09-20 11:19:10 +0700

    refactor: create base card and base card with squares accent; refactor `EventStatCard` and `SymptomCard`;

---
+++ components/Base/Card/Card.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    :class="{
+      'base-card': true,
+      'base-card--block': block,
+      'base-card--bordered': bordered,
+    }"
+    v-on="$listeners"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    block: {
+      type: Boolean
+    },
+    bordered: {
+      type: Boolean
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.base-card {
+  padding: 1.5rem;
+  line-height: 1.618;
+
+  @apply relative z-0
+  inline-block
+  rounded-lg;
+
+  &::v-deep > * {
+    z-index: 1;
+    @apply relative;
+  }
+
+  &--block {
+    @apply block;
+  }
+
+  &--bordered {
+    @apply border border-solid border-gray-300;
+  }
+}
+</style>
