0aa360c6886088e52de32d9cb7bac099d18ec1d5
Arif9878 <arifboyz16@gmail.com>
2021-09-21 11:47:33 +0700

    page bansos

--- pages/info/bansos/index.vue
+++ pages/info/bansos/index.vue
@@ -22,8 +22,7 @@
       </Jumbotron>
     </Section>
     <Section class="py-6 md:py-16 bg-white">
-      <ImageMobile v-if="isMobile" />
-      <ImageDekstop v-else />
+      <img class="md:w-full" :src="imgFlowBansos" alt="content-image-flow-bansos">
     </Section>
     <Section class="py-6 md:py-16 bg-white">
       <SocialAssistanceCard />
@@ -42,38 +41,11 @@ export default {
     Section,
     ContentCard,
     Jumbotron,
-    SocialAssistanceCard,
-    ImageMobile: () => import('./ImageMobile'),
-    ImageDekstop: () => import('./ImageDekstop')
+    SocialAssistanceCard
   },
   data () {
     return {
-      isMobile: false,
-      listSymptom: [
-        {
-          title: 'Demam ≥ 38°C',
-          body: 'Demam merupakan gejala pertama yang diderita sebagian besar pasien Covid-19. Ada kecenderungan pasien mengalami demam selama beberapa hari setelah terkena virus.',
-          image: '../img/icon-fever.svg',
-          color: '#FFB900'
-        },
-        {
-          title: 'Batuk Kering',
-          body: 'Hal ini dikarenakan infeksi virus menyebar ke dalam tubuh melalui hidung atau mulut, sehingga gejala kedua yang dapat menyebabkan timbulnya penyakit ini adalah batuk.',
-          image: '../img/icon-cough.svg',
-          color: '#30a5ff'
-        },
-        {
-          title: 'Sesak Napas',
-          body: 'Berdasarkan studi yang diterbitkan dalam jurnal JAMA menyebutkan bahwa gejala akibat Covid-19 ini bisa bertahan lama. Ini mungkin disebabkan peradangan yang berlangsung cukup lama di paru-paru.',
-          image: '../img/icon-congested.svg',
-          color: '#069550'
-        }
-      ],
-      section: {
-        loading: false,
-        title: 'Gejala Umum Covid-19',
-        subtitle: 'Jika ada orang yang dalam 14 hari sebelum muncul gejala di bawah ini pernah melakukan perjalanan ke negara terjangkit, atau pernah merawat/kontak erat dengan penderita Covid-19, maka terhadap orang tersebut akan dilakukan pemeriksaan laboratorium lebih lanjut untuk memastikan diagnosisnya.'
-      },
+      imgFlowBansos: 'https://firebasestorage.googleapis.com/v0/b/jabarprov-covid19.appspot.com/o/public%2Fdekstop-flow-bansos.png?alt=media&token=69acce3b-9c01-45f4-8ee0-0d311050887b',
       listContent: [
         {
           title: 'Selamat datang di Portal Informasi Data Penerima Bantuan Sosial Kemensos',
