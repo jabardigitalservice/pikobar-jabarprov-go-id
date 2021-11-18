476365ca10c77cfc30a3a23505c5ba058122ce74
maulanayuseph <maulanayuseph@gmail.com>
2021-11-18 12:21:41 +0700

    create a new pages consultation-request

---
+++ pages/consultation-request/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <WelcomePage v-if="showStep === 0" :step.sync="showStep" />
+    <FirstStep v-if="showStep === 1" :step.sync="showStep" />
+    <SecondStep v-if="showStep === 2" :step.sync="showStep" />
+    <ConfirmationStep v-if="showStep === 3" :step.sync="showStep" />
+    <SuccessPage v-if="showStep === 4" :step.sync="showStep" :consultation="true" />
+  </div>
+</template>
+
+<script>
+import WelcomePage from '~/components/_pages/index/VitaminRequest/WelcomePage.vue'
+import FirstStep from '~/components/_pages/index/VitaminRequest/FirstStep.vue'
+import SecondStep from '~/components/_pages/index/VitaminRequest/SecondStep.vue'
+import ConfirmationStep from '~/components/_pages/index/VitaminRequest/ConfirmationStep.vue'
+import SuccessPage from '~/components/_pages/index/VitaminRequest/SuccessPage.vue'
+export default {
+  components: {
+    WelcomePage,
+    FirstStep,
+    SecondStep,
+    ConfirmationStep,
+    SuccessPage
+  },
+  data () {
+    return {
+      showStep: 4
+    }
+  }
+}
+</script>
