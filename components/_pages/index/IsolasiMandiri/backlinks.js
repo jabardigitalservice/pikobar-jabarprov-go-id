8364d8b9f239eb5bdee7869acff1d216d57c58e3
maulanayuseph <maulanayuseph@gmail.com>
2021-11-18 12:20:35 +0700

    create a new const on backlinks file

--- components/_pages/index/IsolasiMandiri/backlinks.js
+++ components/_pages/index/IsolasiMandiri/backlinks.js
@@ -2,10 +2,12 @@ const msg = process.env.NUXT_ENV_ISOMAN_WHATSAPP_MSG
 const phoneNum = process.env.NUXT_ENV_ISOMAN_WHATSAPP_NUMBER
 const konsultasiDokter = `https://wa.me/${phoneNum}?text=${encodeURIComponent(msg)}`
 const permohonanKebutuhan = '/vitamin-request'
+const permohonanKonsultasiDokter = '/consultation-request'
 const trackApplication = '/tracking'
 
 export {
   konsultasiDokter,
   permohonanKebutuhan,
-  trackApplication
+  trackApplication,
+  permohonanKonsultasiDokter
 }
