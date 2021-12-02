61b00df08773c68321d8ec2be626b0b6ce2da276
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2021-12-02 17:14:14 +0700

    Create DocumentSkeleton.vue

---
+++ components/_pages/documents/DocumentSkeleton.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="document-skeleton">
+    <BaseSkeleton
+      class="w-12 h-10 rounded-md"
+    />
+    <div class="document-skeleton__content">
+      <BaseSkeleton
+        class="w-32 h-4 rounded-md"
+      />
+      <BaseSkeleton
+        class="w-1/2 h-4 rounded-md"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseSkeleton from '~/components/Base/Skeleton'
+export default {
+  components: {
+    BaseSkeleton
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.document-skeleton {
+  @apply flex flex-col p-4
+  border border-solid rounded-lg gap-3;
+
+  @screen sm {
+    @apply flex-row items-center;
+  }
+
+  &__content {
+    @apply flex flex-col gap-4 w-full;
+  }
+}
+</style>
