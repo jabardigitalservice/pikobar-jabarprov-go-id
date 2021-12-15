e999a4341ecedfe8d42bddb417a1e2e5fe13a4bd
Naufal Ihsan Kusumayadhi <56071566+naufalihsank@users.noreply.github.com>
2021-12-15 11:37:53 +0700

    add extra enter

--- api/documents.js
+++ api/documents.js
@@ -53,7 +53,7 @@ export function getById (id) {
           id: doc.id,
           published_at: data.published_at.toDate(),
           route,
-          shareText: `[Pikobar] Bagikan "${data.title}". Selengkapnya di ${process.env.URL}${route}`
+          shareText: `[Pikobar] Bagikan "${data.title}".\n\nSelengkapnya di ${process.env.URL}${route}`
         }
       }
       return null
