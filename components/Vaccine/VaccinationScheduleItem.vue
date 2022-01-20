7901dba77087a48709f14337885c0d025719c37e
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2022-01-20 12:07:40 +0700

    style (vaccination-schedule): Push lihat selengkapnya into bottom card

--- components/Vaccine/VaccinationScheduleItem.vue
+++ components/Vaccine/VaccinationScheduleItem.vue
@@ -1,28 +1,30 @@
 <template>
   <div class="schedule-item">
-    <span class="schedule-item__header">
-      {{ header }}
-    </span>
-    <span class="schedule-item__title">
-      {{ title }}
-    </span>
-    <span class="schedule-item__address">
-      {{ cutString(address) }}
-    </span>
-    <div class="flex flex-row gap-2 mb-6">
-      <img :src="iconClock">
-      <div class="schedule-item__date">
-        {{ date }}
-      </div>
-    </div>
-    <div class="flex flex-row flex-wrap gap-2">
-      <span
-        v-for="age in ageCategory"
-        :key="age"
-        class="schedule-item__age"
-      >
-        {{ age }}
+    <div class="flex flex-col items-start">
+      <span class="schedule-item__header">
+        {{ header }}
       </span>
+      <span class="schedule-item__title">
+        {{ title }}
+      </span>
+      <span class="schedule-item__address">
+        {{ cutString(address) }}
+      </span>
+      <div class="flex flex-row gap-2 mb-6">
+        <img :src="iconClock">
+        <div class="schedule-item__date">
+          {{ date }}
+        </div>
+      </div>
+      <div class="flex flex-row flex-wrap gap-2">
+        <span
+          v-for="age in ageCategory"
+          :key="age"
+          class="schedule-item__age"
+        >
+          {{ age }}
+        </span>
+      </div>
     </div>
     <span class="schedule-item__show-more">
       Lihat Selengkapnya
@@ -74,7 +76,7 @@ export default {
 <style lang="scss" scoped>
 .schedule-item {
   @apply flex flex-col bg-brand-gray
-  items-start p-4 rounded-lg;
+  justify-between p-4 rounded-lg;
 
   &__header {
     @apply font-lato text-sm text-white
