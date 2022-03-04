55773e64de6c89dada7640fabfceafae6d7dae9c
Yuseph Maulana <maulanayuseph@gmail.com>
2022-03-04 18:00:34 +0700

    chore: change to personal identity step at the first click consultation request

--- pages/consultation-request/index.vue
+++ pages/consultation-request/index.vue
@@ -1,16 +1,15 @@
 <template>
   <div>
-    <WelcomePage v-if="showStep === 0" :step.sync="showStep" consultation />
     <FirstStep v-if="showStep === 1" :step.sync="showStep" />
     <SecondStep v-if="showStep === 2" :step.sync="showStep" />
     <ThirdStep v-if="showStep === 3" :step.sync="showStep" />
-    <ConfirmationStep v-if="showStep === 4" :step.sync="showStep" />
-    <SuccessPage v-if="showStep === 5" :step.sync="showStep" consultation />
+    <PreviewImageStep v-if="showStep === 4" :step.sync="showStep" />
+    <ConfirmationStep v-if="showStep === 5" :step.sync="showStep" />
+    <SuccessPage v-if="showStep === 6" :step.sync="showStep" consultation />
   </div>
 </template>
 
 <script>
-import WelcomePage from '~/components/_pages/index/VitaminRequest/WelcomePage.vue'
 import FirstStep from '~/components/_pages/index/ConsultationVitamin/First.vue'
 import SecondStep from '~/components/_pages/index/ConsultationVitamin/Second.vue'
 import ThirdStep from '~/components/_pages/index/ConsultationVitamin/Third.vue'
@@ -18,7 +17,6 @@ import ConfirmationStep from '~/components/_pages/index/VitaminRequest/Confirmat
 import SuccessPage from '~/components/_pages/index/VitaminRequest/SuccessPage.vue'
 export default {
   components: {
-    WelcomePage,
     FirstStep,
     SecondStep,
     ThirdStep,
@@ -27,7 +25,7 @@ export default {
   },
   data () {
     return {
-      showStep: 0
+      showStep: 3
     }
   }
 }
