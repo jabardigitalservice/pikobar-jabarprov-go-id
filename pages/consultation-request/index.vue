bbcebfc83f45d8da9d2e3f37f98634a8111ac203
Yuseph Maulana <maulanayuseph@gmail.com>
2022-03-07 15:42:14 +0700

    chore: add preview data component on page

--- pages/consultation-request/index.vue
+++ pages/consultation-request/index.vue
@@ -3,7 +3,7 @@
     <FirstStep v-if="showStep === 1" :step.sync="showStep" />
     <SecondStep v-if="showStep === 2" :step.sync="showStep" />
     <ThirdStep v-if="showStep === 3" :step.sync="showStep" />
-    <ConfirmationStep v-if="showStep === 4" :step.sync="showStep" />
+    <PreviewData v-if="showStep === 4" :step.sync="showStep" />
     <SuccessPage v-if="showStep === 5" :step.sync="showStep" consultation />
   </div>
 </template>
@@ -12,14 +12,14 @@
 import FirstStep from '~/components/_pages/index/ConsultationVitamin/First.vue'
 import SecondStep from '~/components/_pages/index/ConsultationVitamin/Second.vue'
 import ThirdStep from '~/components/_pages/index/ConsultationVitamin/Third.vue'
-import ConfirmationStep from '~/components/_pages/index/VitaminRequest/ConfirmationStep.vue'
+import PreviewData from '~/components/_pages/index/ConsultationVitamin/PreviewData.vue'
 import SuccessPage from '~/components/_pages/index/VitaminRequest/SuccessPage.vue'
 export default {
   components: {
     FirstStep,
     SecondStep,
     ThirdStep,
-    ConfirmationStep,
+    PreviewData,
     SuccessPage
   },
   data () {
