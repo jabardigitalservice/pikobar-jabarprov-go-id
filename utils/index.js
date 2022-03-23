ef314df46ddb74b902cc02834f439c7d1e81bd37
Yuseph Maulana <maulanayuseph@gmail.com>
2022-03-23 16:18:17 +0700

    chore: add utils check is mobile phone

--- utils/index.js
+++ utils/index.js
@@ -39,5 +39,12 @@ export default {
       default:
         return []
     }
+  },
+  checkIsMobile () {
+    let isMobile = false
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      isMobile = true
+    }
+    return isMobile
   }
 }
