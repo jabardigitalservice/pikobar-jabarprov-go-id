6b36e682705e2069fcde496651ba42736f089078
Yuseph Maulana <maulanayuseph@gmail.com>
2022-03-28 10:06:10 +0700

    chore: revamp section hero in isoman page

--- pages/isoman/index.vue
+++ pages/isoman/index.vue
@@ -1,6 +1,9 @@
 <template>
   <!-- eslint-disable vue/html-closing-bracket-newline -->
   <div>
+    <Section class="py-6 md:py-20 bg-white">
+      <ContentCard v-bind="headerContent" />
+    </Section>
     <section class="bg-brand-green-darker">
       <div class="container mx-auto">
         <div class="mx-4 flex flex-wrap gap-8 py-12 justify-between">
@@ -33,12 +36,29 @@
 </template>
 
 <script>
-import IsolasiMandiri from '../../components/_pages/index/IsolasiMandiri'
-import Testimonial from '../../components/_pages/testimonial'
+import IsolasiMandiri from '~/components/_pages/index/IsolasiMandiri'
+import Testimonial from '~/components/_pages/testimonial'
+import ContentCard from '~/components/Base/ContentCard'
+import { Section } from '~/components/Base/Section'
 export default {
   components: {
     IsolasiMandiri,
-    Testimonial
+    Testimonial,
+    ContentCard,
+    Section
+  },
+  data () {
+    return {
+      headerContent: {
+        headerSize: 'large',
+        title: 'Pasien Omicron Boleh Isoman, Tapi Harus Tahu Hal ini!',
+        body: 'Baca sebentar yuk terkait; syarat isoman, panduan isoman, yang harus dilakukan pada saat kontak dengan pasien omicron, dan yang harus dilakukan setelah selesai isoman.',
+        image: require('~/assets/illustrations/isoman.svg'),
+        imagePosition: 'right',
+        prompt: 'Selengkapnya',
+        backLink: '#section-health-services'
+      }
+    }
   },
   mounted () {
     this.$store.dispatch('self-isolation/getInfoItems')
