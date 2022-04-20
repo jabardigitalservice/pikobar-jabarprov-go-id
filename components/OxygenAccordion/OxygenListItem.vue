278814d4e2cfb7a0f69a9acb07f89bd4315e4a70
Yuseph Maulana <maulanayuseph@gmail.com>
2022-04-20 12:07:04 +0700

    chore: revamp style and icons list location tube oxygen

--- components/OxygenAccordion/OxygenListItem.vue
+++ components/OxygenAccordion/OxygenListItem.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="mb-8 pl-5 relative">
-    <div class="absolute top-0 bottom-0 left-0 w-1 bg-orange-300" />
+  <div class="bg-gray-100 h-40 mb-8 pl-5 relative">
     <h5 class="inline-block text-lg font-bold leading-loose">
       {{ oxygen_center_name }}
     </h5>
@@ -12,23 +11,28 @@
         <a
           v-for="(num, j) in phones"
           :key="j"
-          class="inline-block px-4 py-1 bg-blue-100 rounded mt-2 mr-2 text-gray-800 hover:opacity-50"
+          class="inline-block px-4 py-1 bg-green-600 rounded-lg mt-2 mr-2 text-white hover:opacity-50"
           :href="`tel:${num}`"
           target="_blank"
           :title="`Telpon ${num}`"
         >
-          <i class="fas fa-phone fa-sm mr-1" />
           <span>
+            <img
+              src="~/assets/icons/phone.svg"
+              width="12"
+              height="12"
+              alt="phone"
+              class="inline"
+            >
             {{ num }}
           </span>
         </a>
       </template>
       <a
         v-if="item_type"
-        class="inline-block px-4 py-1 bg-green-100 rounded mt-2 mr-2 text-gray-800 hover:opacity-50"
+        class="inline-block px-4 py-1 bg-blue-600 rounded-lg mt-2 mr-2 text-white hover:opacity-50"
         :title="`Buka Laman ${item_type}`"
       >
-        <i class="fas fa-globe-asia fa-sm mr-1" />
         <span>
           {{ item_type }}
         </span>
