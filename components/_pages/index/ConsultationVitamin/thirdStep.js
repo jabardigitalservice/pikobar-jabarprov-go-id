445813f2daf0a16e1beff0ba995fa8552b30feb7
maruf12 <maruf.harsono@gmail.com>
2022-12-06 15:34:31 +0700

    delete pdf from upload file component

--- components/_pages/index/ConsultationVitamin/thirdStep.js
+++ components/_pages/index/ConsultationVitamin/thirdStep.js
@@ -10,8 +10,8 @@ export default [
     required: true,
     model: 'ktp_photo',
     name: 'KTP',
-    rules: 'required|mimes:png,jpg,jpeg,pdf|size:5000',
-    accept: '.png,.jpg,.jpeg,.pdf',
+    rules: 'required|mimes:png,jpg,jpeg|size:5000',
+    accept: '.png,.jpg,.jpeg',
     class: 'md:block md:w-8/12',
     type: 'file'
   },
@@ -20,8 +20,8 @@ export default [
     required: true,
     model: 'test_result_photo',
     name: 'Foto Hasil Tes',
-    rules: 'required|mimes:png,jpg,jpeg,pdf|size:5000',
-    accept: '.png,.jpg,.jpeg,.pdf',
+    rules: 'required|mimes:png,jpg,jpeg|size:5000',
+    accept: '.png,.jpg,.jpeg',
     class: 'md:block md:w-8/12',
     type: 'file'
   }
