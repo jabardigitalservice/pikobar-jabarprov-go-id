6148202818dcff72b3174a13c90a898f124e5f08
maruf12 <maruf.harsono@gmail.com>
2023-01-12 16:56:16 +0700

    refactor redirect

--- middleware/redirect.js
+++ middleware/redirect.js
@@ -1,38 +1,56 @@
 export default function ({ redirect, route }) {
   // Redirect to /isoman when user access feature request vitamin
-  if (route.path === '/isoman/consultation-request') {
-    return redirect('/isoman')
-  }
+  const routeList = [
+    {
+      path: '/isoman/consultation-request',
+      to: '/isoman'
+    },
+    {
+      path: '/vitamin-request',
+      to: '/isoman'
+    },
+    {
+      path: '/consultation-request',
+      to: '/isoman'
+    },
+    {
+      path: '/tracking',
+      to: '/isoman'
+    },
+    {
+      path: '/oxygen',
+      to: '/isoman'
+    },
+    {
+      path: '/data',
+      to: 'statistic'
+    },
+    {
+      path: '/distribution-case',
+      to: 'statistic'
+    },
+    {
+      path: '/distribution-healthcare',
+      to: 'healtcare'
+    },
+    {
+      path: '/table-case',
+      to: 'table-case'
+    }
+  ]
 
-  if (route.path === '/vitamin-request') {
-    return redirect('/isoman')
-  }
-
-  if (route.path === '/consultation-request') {
-    return redirect('/isoman')
-  }
-
-  if (route.path === '/tracking') {
-    return redirect('/isoman')
-  }
-
-  if (route.path === '/oxygen') {
-    return redirect('/isoman')
-  }
-
-  if (route.path === '/data') {
-    return redirect(process.env.URL_STATISTIK)
-  }
-
-  if (route.path === '/distribution-case') {
-    return redirect(process.env.URL_STATISTIK)
-  }
-
-  if (route.path === '/distribution-healthcare') {
-    return redirect(process.env.URL_FASYANKES_TREATMENT)
-  }
-
-  if (route.path === '/table-case') {
-    return redirect(process.env.URL_TABLE_CASE)
-  }
+  routeList.forEach((el) => {
+    if (route.path === el.path) {
+      switch (el.to) {
+        case 'table-case':
+          return redirect(process.env.URL_TABLE_CASE)
+        case 'healtcare':
+          return redirect(process.env.URL_FASYANKES_TREATMENT)
+        case 'statistic':
+          return redirect(process.env.URL_STATISTIK)
+        case '/isoman':
+          return redirect(el.to)
+      }
+    }
+  })
 }
